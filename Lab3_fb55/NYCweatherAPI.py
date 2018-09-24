429f4f209e2c99adb779016e63b9cf7fdda9cc8d
Hancylee525 <41362791+Hancylee525@users.noreply.github.com>
2018-09-23 20:46:25 -0400

    Update NYCweatherAPI.py

--- Lab3_fb55/NYCweatherAPI.py
+++ Lab3_fb55/NYCweatherAPI.py
@@ -1,19 +1,19 @@
-from __future__ import print_function
-import json
-try:
+from __future__ import print_function#python 3引到2
+import json #包
+try:#改包的名字
     import urllib2 as urllib
 except ImportError:
     import urllib.request as urllib
-import os
-import sys
+import os# 管理文件路径的python包 读取文件路径 环境变量
+import sys #管理参数
 
 
-apikey = os.getenv("WEATHERAPIKEY")
+apikey = os.getenv("WEATHERAPIKEY")#api内置
 mode = "Json"
 units = "metric"
 city = sys.argv[1]
 
-url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&mode=%s&units=%s&cnt=7&APPID=%s"%(city, mode, units, apikey)
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&mode=%s&units=%s&cnt=7&APPID=%s"%(city, mode, units, apikey)#分装成的样式
 
 print (url)
 response = urllib.urlopen(url)
